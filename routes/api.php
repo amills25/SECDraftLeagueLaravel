b9d144173a0a59df829b93b1c047f4758f5bd670
awmills25 <awmills25@gmail.com>
2021-11-11 13:07:41 -0500

    through dary video 36

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware('auth:api')->prefix('v1')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
