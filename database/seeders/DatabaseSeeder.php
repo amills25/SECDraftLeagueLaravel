9bb2bbeb99a4f984d7bd31e917a13a3629321d57
awmills25 <awmills25@gmail.com>
2021-11-30 20:35:59 -0500

    blog post table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(MembershipSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(UserMembershipSeeder::class);
+        $this->call(BlogPostSeeder::class);
     }
 }
