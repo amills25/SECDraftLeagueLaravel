82d14d38bc2e02508a37d32c2ecdb480d2479e44
awmills25 <awmills25@gmail.com>
2021-12-09 12:03:37 -0500

    updated pathing

--- app/Http/Controllers/LineupController.php
+++ app/Http/Controllers/LineupController.php
@@ -78,7 +78,7 @@ class LineupController extends Controller
         //loop through rows and adjust the athlete
         $lineup = Lineup::find($request->lineup_id);
 
-        for ($i = 1; $i <= 9; $i++) {
+        for ($i = 0; $i < 9; $i++) {
             $athlete = Athlete::find($request->rows[$i]['active']['props']['athleteID']);
 
             $athlete->name = $request->rows[$i]['name'];
@@ -86,19 +86,19 @@ class LineupController extends Controller
             $athlete->active = $request->rows[$i]['toggled'];
 
             $athlete->save();
+            //loop through rows and adjust the week
+            for ($j = 0; $j < 10; $j++) {
+                $week = Week::find($j);
+
+                $week->athlete_id = $request->rows[$i]['active']['props']['athleteID'];
+                // $week->lineup_id = $request[$i]->lineup_id;
+                $week->points = $request->rows[$i]['week' . ($j + 1)];
+                // $week->week_number = $request[$i]->week_number;
+
+                $week->save();
+            }
         }
 
-        //loop through rows and adjust the week
-        for ($i = 1; $i <= 10; $i++) {
-            $week = Week::find($request->wk_id);
-
-            $week->athlete_id = $request[$i]->athlete_id;
-            $week->lineup_id = $request[$i]->lineup_id;
-            $week->points = $request[$i]->points;
-            $week->week_number = $request[$i]->week_number;
-
-            $week->save();
-        }
 
         $lineup->save();
     }
